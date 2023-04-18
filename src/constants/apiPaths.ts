3edb952db89ebe3e3eef022a06e63ee5b0f93e49
Aleh Harnizonau <aleh.harnizonau@sap.com>
2023-04-18 17:08:40 +0300

    chore: fix paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://6gzvhb5guj.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://rb05wuzhm4.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
